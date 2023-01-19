0d580eb41fd57dc24982e199bd5b0d653a51466a
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2023-01-19 01:32:39 -0300

    cookies, politica privacidade, responsividade

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const navbar_oculta = document.getElementById("navbar-oculta")
 const navbar_oculta_container = document.getElementById("navbar-oculta__container")
 
+showCookiesDiv()
+
+
 function onBodyScroll(){
     if(window.pageYOffset > window.innerHeight){
         navbar_oculta.classList.add("ativo")
-        navbar_oculta_container.classList.add("ativo")
     }
     else{
         navbar_oculta.classList.remove("ativo")
-        navbar_oculta_container.classList.remove("ativo")
     }
 }
 
@@ -19,7 +20,6 @@ function onClickBotaoVerPlanos(){
     const offset = div_orcamento.getBoundingClientRect().top - document.body.getBoundingClientRect().top - 80 //esse 80 eh a altura da navbar, se deixar pra usar o .offsetHeight, o botao da capa nao vai funcionar pois na hora q ele for pressionado, a navbar terah altura 0
     
     window.scrollTo({top: offset, behavior: 'smooth'})
-    console.log(offset)
 }
 
 
@@ -32,3 +32,51 @@ function onClickDivPergunta(_id){
     const div_pergunta = document.getElementById(_id)
     div_pergunta.classList.toggle("ativo")
 }
+
+
+function onClickBotaoWhatsApp(){
+    gtag_report_conversion()
+}
+
+function gtag_report_conversion(url) {
+    var callback = function () {
+      if (typeof(url) != 'undefined') {
+        window.location = url;
+      }
+    };
+    gtag('event', 'conversion', {
+        'send_to': 'AW-379172488/aaIhCIGDhYgYEIjt5rQB',
+        'event_callback': callback
+    });
+    return false;
+}
+
+
+function showCookiesDiv(){
+    let cookiesLength = document.cookie.length
+    if(cookiesLength === 0){
+        document.getElementById("main__div-cookies").classList.add("ativo")
+    }
+    else{
+        document.getElementById("main__div-cookies").classList.remove("ativo")
+    }
+}
+
+
+function onClickBotaoCookiesVerConfig(){
+    window.open("./pagina-politica-privacidade.html", '_blank');
+}
+
+
+function onClickBotaoCookiesAceito(){
+    document.cookie = "permissaoCookies=permitido; path=/"
+    document.getElementById("main__div-cookies").classList.remove("ativo")
+}
+
+
+
+
+
+
+
+
